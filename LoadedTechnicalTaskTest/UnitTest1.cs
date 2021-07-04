39e992eae0e9cd90423e25827f16db1cb0049eb8
xingtong <x1980p@gmail.com>
2021-07-04 12:34:47 +1200

    Add unit test project

---
+++ LoadedTechnicalTaskTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LoadedTechnicalTaskTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
